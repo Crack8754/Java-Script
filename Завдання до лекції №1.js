6259468e14b05a3eafda14f1c4a6925fe0a113f2
Crack8754 <puyadmitriy98@gmail.com>
2024-09-16 15:40:34 +0300

    Create Завдання до лекції №1.js

---
+++ Завдання до лекції №1.js
@@ -0,0 +1,35 @@
+alert('Hello world');
+
+//Task 1
+let a = +prompt('Input a:');
+let b = +prompt('Input b:');
+let operation = prompt('Input operation:');
+
+if ( operation === "-")
+{
+	alert(a - b);
+}
+//Task 2
+if ( operation === "*" ) {
+	alert(a * b);
+}
+//Task 3
+let a = prompt('Input a:');
+let b = prompt('Input b:');
+
+alert(a + b);
+//Task 4
+let a = prompt('Input a:');
+
+alert(a.length);
+//Task 5
+let a = prompt('Input a:');
+
+alert(a.toUpperCase());
+//Task 6
+const array1 = ['1', '2', '3'];
+console.log('array1:', array1);
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+
